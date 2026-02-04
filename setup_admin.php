82c420dd5b0b63157bdad5969f1bb3bf2f1eec96
mira <meermohammed80@gmail.com>
2026-02-04 05:07:23 +0300

    Prepared

--- setup_admin.php
+++ setup_admin.php
@@ -7,9 +7,9 @@
 
 require_once __DIR__ . '/db.php';
 
-$username = 'MirMohammedRashid';
-$password = 'Mira19$==';
-$email = 'meermohammed80@gmail.com';
+$username = 'Admin';
+$password = 'admin123';
+$email = 'admin@gmail.com';
 
 // Generate proper bcrypt hash
 $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
